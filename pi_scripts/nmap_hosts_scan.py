9a48628700abbae75b30740c516ec3a95d94af6b
RickyDZN <tigandenis@gmail.com>
2025-09-29 13:32:59 +0300

    hosts scan

---
+++ pi_scripts/nmap_hosts_scan.py
@@ -0,0 +1,44 @@
+import nmap
+
+
+def nmap_hosts_scan(ip_range):
+
+    scanner = nmap.PortScanner()
+    options = "-sn -PR"  # Ping scan
+    """
+    -sn: Ping scan
+    -PR: ARP ping
+    -R:  reverse DNS
+    """
+    print("---> Starting scanning network...")
+    print("...")
+    print("...")
+
+    scanner.scan(hosts=ip_range, arguments=options)
+
+    hosts = scanner.all_hosts()
+
+    results = []
+    for host in hosts:
+        #print(scanner[host]['addresses'])
+        #print(scanner[host]['hostnames'])
+        #results[host] = scanner[host]['hostnames']
+
+        results.append ({
+            "ip": scanner[host]['addresses'].get('ipv4') or "Unknown",
+            "mac": scanner[host]['addresses'].get('mac') or "Unknown"
+        })
+
+    print("--->Scan complete")
+    print(f"--->Found {len(results)} hosts\n")
+
+    print("--->Devices:")
+    for result in results:
+        print(f"IP: {result['ip']}, MAC: {result['mac']}")
+
+    print("==========================================\n")
+
+    return results
+
+if __name__ == "__main__":
+    nmap_hosts_scan("192.168.0.0/24")
